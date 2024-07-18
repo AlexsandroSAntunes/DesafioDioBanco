60576fc951fd79316bd2628c9f30b09d450adf6d
Alexsandro <Alexsandro@DESKTOP-V5TCQHF>
2024-07-17 22:10:45 -0300

    DesafioDIO

---
+++ src/ContaCorrente.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alexasandro
+ */
+public class ContaCorrente {
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar(){
+    }
+    public void depositar(){
+    }
+    public void transferir(){
+}
+}    

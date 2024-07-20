1573a7da5b4e38f2751912c87853678709220eef
Alexsandro <Alexsandro@DESKTOP-V5TCQHF>
2024-07-19 22:09:11 -0300

    DesafioDioBanco

---
+++ src/DesafioDiobBanco/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DesafioDiobBanco;
+
+/**
+ *
+ * @author bgbgbgbg
+ */
+public class Main {
+    public static void main(String[]args){
+        Conta cc = new ContaCorrente();
+        Conta poupanca = new ContaPoupanca();
+        System.out.println();
+    }
+}
